f24565c832e1158ca107826c6a9ba76a9795e892
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-31 16:52:56 -0400

    updated pokemon card typing

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,17 +1,46 @@
 import '/src/styles/Cards.css'
 
+const pokemonTypeColors = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  grass: "#7AC74C",
+  electric: "#F7D02C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD"
+};
+
 function Card({ name, img, hp, types, onClick }) {
   return (
     <div className="pokemon-card" onClick={onClick}>
       <div className="card-header">
-        <span>{name}</span>
+        <span><b>{name}</b></span>
         <span>HP: {hp}</span>
       </div>
       <div className="pokemon-image">
         <img src={img} alt={name} />
       </div>
       <div className="pokemon-info">
-        <p>Type: {types.length === 1 ? `${types[0]}` : `${types[0]}, ${types[1]}` }</p>
+      <p>Type: {types.map(type => (
+          <span 
+            key={type}
+            className='pokemon-type' 
+            style={{backgroundColor: pokemonTypeColors[type]}}
+          >
+            {type}
+          </span>
+        ))}</p>
         <p>Attack 1: Example Attack</p>
         <p>Attack 2: Another Attack</p>
       </div>
