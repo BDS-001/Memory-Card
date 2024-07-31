365909c3b8a80ce7757675119e8ee9f15e12c395
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-30 21:37:43 -0400

    removed vite and react starter code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
 import { v4 as uuid } from 'uuid';
+import CardList from './Cards';
 import '../styles/App.css'
 
 function App() {
@@ -19,30 +18,12 @@ function App() {
       return shuffled;
   }
 
-  const [cards, setCards] = useState([])
+  const [cards, setCards] = useState(shuffleCards(initialCards))
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header><h1>Memory Game</h1></header>
+      <CardList cards={cards} />
     </>
   )
 }
